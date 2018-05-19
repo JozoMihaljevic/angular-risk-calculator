33f4097c30e497a7e2fabd0809b8fa697edfd953
INTRA\jozo.mihaljevic <jozo.mihaljevic1@gmail.com>
2018-05-19 15:31:15 +0200

    Neke izmjene u bmi.service, uvjeti bmp i bmi, izracun bmp.

--- src/app/bmi.service.ts
+++ src/app/bmi.service.ts
@@ -21,7 +21,7 @@ stupanjBFP: number;
   	let bodyFatPostotak;
 
     if (osobni.spol == 'm') {
-      bodyFatPostotak = ((1.2 * bmi) + (0.23 * osobni.dob) - 5.4 - (10.8 * 1)).toFixed(0);
+      bodyFatPostotak = ((1.2 * bmi) + (0.23 * osobni.dob) - 5.4 - 10.8).toFixed(0);
     } else {
       bodyFatPostotak = ((1.2 * bmi) + (0.23 * osobni.dob) - 5.4).toFixed(0);
     }
@@ -39,26 +39,19 @@ stupanjBFP: number;
 		if (bmi <= 18.4) {
       porukaBmi = 'Tjelesna masa ispod preporučene, rizik u odnosu na BMI: Nizak';
       this.stupanjBMI = 0; 
-    } else if (18.5 <= bmi && bmi < 24.9) {
+    } else if (18.4 < bmi && bmi <= 24.9) {
       porukaBmi = 'Idealna tjelesna masa, rizik u odnosu na BMI: Normalan';
       this.stupanjBMI = 0;
-    } else if (25 <= bmi && bmi < 29.9) {
+    } else if (24.9 < bmi && bmi <= 29.9) {
       porukaBmi = 'Povišena tjelesna masa, rizik u odnosu na BMI: Povišen';
       this.stupanjBMI = 1;
-    } else if (30.0 <= bmi && bmi < 34.9) {
+    } else if (29.9 < bmi && bmi <= 34.9) {
       porukaBmi = 'Prekomjerna tjelesna masa (gojaznost, pretilost) 1. stupnja!!! Rizik u odnosu na BMI: Visok';
       this.stupanjBMI = 2;
-    } else if (35 <= bmi && bmi < 45) {
+    } else if (34.9 < bmi && bmi <= 45) {
       porukaBmi = 'Prekomjerna tjelesna masa (gojaznost, pretilost) 2. stupnja!!! Rizik u odnosu na BMI: Visok';
       this.stupanjBMI = 3;
-    } else if (35 <= bmi && bmi < 45) {
-      porukaBmi = 'Prekomjerna tjelesna masa (gojaznost, pretilost) 2. stupnja!!! Rizik u odnosu na BMI: Visok';
-      this.stupanjBMI = 4;
-    } else if (45 <= bmi) {
-      porukaBmi = 'Prekomjerna tjelesna masa (gojaznost, pretilost) 3. stupnja!!! Rizik u odnosu na BMI: Visok';
-      this.stupanjBMI = 4;
-    }
-      else {
+    } else {
       porukaBmi = 'Prekomjerna tjelesna masa (gojaznost, pretilost) 3. stupnja!!! Rizik u odnosu na BMI: Visok';
       this.stupanjBMI = 4;
     }
@@ -69,19 +62,19 @@ stupanjBFP: number;
   	let porukaBodyFatPostotak: string;
   	// za muškarce:
     if (osobni.spol == 'm') {
-      if (bodyFatPostotak < 6) {
+      if (bodyFatPostotak <= 6) {
         porukaBodyFatPostotak = 'Rizik za nastanak kardiovaskularnih bolesti u odnosu na postotak tjelesnih masti: Nizak';
         this.stupanjBFP = 0;
-      } else if (7 < bodyFatPostotak && bodyFatPostotak < 13) {
+      } else if (7 <= bodyFatPostotak && bodyFatPostotak <= 13) {
         porukaBodyFatPostotak = 'Rizik za nastanak kardiovaskularnih bolesti u odnosu na postotak tjelesnih masti: Nizak';
         this.stupanjBFP = 0;
-      } else if (14 < bodyFatPostotak && bodyFatPostotak < 17) {
+      } else if (14 <= bodyFatPostotak && bodyFatPostotak <= 17) {
         porukaBodyFatPostotak = 'Rizik za nastanak kardiovaskularnih bolesti u odnosu na postotak tjelesnih masti: Normalan';
         this.stupanjBFP = 1;
-      } else if (18 < bodyFatPostotak && bodyFatPostotak < 22) {
+      } else if (18 <= bodyFatPostotak && bodyFatPostotak <= 22) {
         porukaBodyFatPostotak = 'Rizik za nastanak kardiovaskularnih bolesti u odnosu na postotak tjelesnih masti: Povišen';
         this.stupanjBFP = 2;
-      } else if (23 < bodyFatPostotak && bodyFatPostotak < 29) {
+      } else if (23 <= bodyFatPostotak && bodyFatPostotak <= 29) {
         porukaBodyFatPostotak = 'Rizik za nastanak kardiovaskularnih bolesti u odnosu na postotak tjelesnih masti: Povišen';
         this.stupanjBFP = 3;
       } else {
